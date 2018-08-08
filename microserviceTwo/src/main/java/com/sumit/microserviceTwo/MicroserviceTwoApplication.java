d78b8c78d6a4576f9f9579c64fd02fdcdcad77fe
Sumit Yadao <sumityadao.18@gmail.com>
2018-08-08 20:35:00 +0530

    Added missing files for second micro service

---
+++ microserviceTwo/src/main/java/com/sumit/microserviceTwo/MicroserviceTwoApplication.java
@@ -0,0 +1,12 @@
+package com.sumit.microserviceTwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceTwoApplication.class, args);
+	}
+}
