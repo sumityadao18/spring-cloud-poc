23ee284e4a9f522d0e8e2b695ef3e21421554f31
Sumit Yadao <sumityadao.18@gmail.com>
2018-08-09 10:32:27 +0530

    Added Eureka Server

--- microserviceOne/src/main/java/com/sumit/microserviceOne/MicroserviceOneApplication.java
+++ microserviceOne/src/main/java/com/sumit/microserviceOne/MicroserviceOneApplication.java
@@ -2,7 +2,9 @@ package com.sumit.microserviceOne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MicroserviceOneApplication {
 
